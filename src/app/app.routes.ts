afbdb6df2d97786814ca9543de9acfb307a8fb65
Sharik-oss <mamukabigvava2201@gmail.com>
2024-12-22 17:06:25 +0400

    second-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainPageComponent
+    },
+    {
+        path: 'congratulations',
+        component: SecondPageComponent
+    }
+];
